51fd46a1562568a61bf015641f295156f4a98b82
jrwithahoodie <jr.gxllego@proton.me>
2024-06-14 17:30:10 +0200

    [ADD] - new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+];
